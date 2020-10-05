9e75e50ca5367fae3e719e4722fb18b31fe27e49
Dilshod Bakhtiyorov <bakhtiyorovdilshod440@gamil.com>
2020-10-06 01:53:36 +0500

    User and Account apps models are ready

---
+++ account/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Account(models.Model):
+    gender = [
+        ('male', 'male'),
+        ('female', 'female')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    gender = models.CharField(
+        max_length=6, choices=gender, default=None, blank=True, null=True)
---
+++ user/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, phone_number, password, **extra_fields):
+        now = timezone.now()
+        if not phone_number:
+            raise ValueError('The given phone_number must be set')
+        user = self.model(phone_number=phone_number, is_active=True, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, password=None, is_provider=None, **extra_fields):
+        return self._create_user(phone_number, password, **extra_fields)
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        return self._create_user(phone_number, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(max_length=254, unique=True)
+    is_director = models.BooleanField(default=False)
+    is_accountant = models.BooleanField(default=False)
+    is_warehouseman = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
